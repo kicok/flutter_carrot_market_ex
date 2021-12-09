b7ef2a847a7754d1e0072db9e5bf56055dbc8f64
kicok <kicoker@gmail.com>
2021-12-09 17:33:38 +0900

    add contentsDetail and CustomScrollView detail

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -145,11 +145,134 @@ class _DetailContentViewState extends State<DetailContentView> {
     );
   }
 
-  Widget _bodyWidget() {
+  Widget _line() {
+    return Container(
+      height: 1,
+      color: Colors.grey.withOpacity(0.3),
+      margin: const EdgeInsets.symmetric(vertical: 15),
+    );
+  }
+
+  Widget _sizedBox15() {
+    return const SizedBox(height: 15);
+  }
+
+  Widget _otherSellContents() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        Text(
+          '판매자짐의 판매 상품',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '모두보기',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Widget _contentsDetail() {
     return Column(
+      // CrossAxisAlignment.start로 했지만 왼쪽 정렬이 되지 않고 중앙에 정렬되있어서 전체의 공간을 활용하기 위해 stretch로 사용함
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+
       children: [
-        _makeSliderImage(),
-        _sellerSimpleInfo(),
+        Text(
+          widget.data['title'].toString(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        const Text(
+          '디지털/가전 ・ 22시간 전',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 12,
+          ),
+        ),
+        _sizedBox15(),
+        const Text(
+          '선물받은 새 상품이고\n상품 꺼내보기만 했습니다\n거래는 직거래만 합니다',
+          style: TextStyle(
+            fontSize: 15,
+            height: 1.5,
+          ),
+        ),
+        _sizedBox15(),
+        const Text(
+          '채팅 3 ・ 관심 17 ・ 조회 205',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _bodyWidget() {
+    // SingleChildScrollView 는 ListView()화 같이 사용할수 없다.
+    // 왜냐하면 SingleChildScrollView()와 ListView()에 모두 scroll속성이 들어있기 때문이다
+    // 그래서 아래와 같은 CustomScrollView 를 사용하고 SliverList()를 사용하는것이다.
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildListDelegate([
+            _makeSliderImage(),
+            _sellerSimpleInfo(),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: [
+                  _line(),
+                  _contentsDetail(),
+                  _line(),
+                  _otherSellContents(),
+                ],
+              ),
+            ),
+          ]),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          sliver: SliverGrid(
+            //gridDelegate: 크기와 위치를 제어하는 delegate
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+            ),
+            // delegate : 여러 children을 2차원 배열로 배치하는 sliver를 만든다.
+            delegate: SliverChildListDelegate(
+              List.generate(20, (index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        height: 120,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const Text(
+                      '상품 제목',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    const Text(
+                      '금액',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                );
+              }),
+            ),
+          ),
+        )
       ],
     );
   }
